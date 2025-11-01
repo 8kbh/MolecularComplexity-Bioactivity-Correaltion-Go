462b43db3d1871e6e5f4f3c463867dbd0df993ad
Oleg K <13.meilleur@gmail.com>
2025-11-01 17:00:45 +0300

    add columns selector

--- main.go
+++ main.go
@@ -10,12 +10,14 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
-func processingSSE(input_fp string, number_of_attempts, sample_size, batch_size, keep_top int, progressChan chan<- int) {
+func processingSSE(input_fp string, smiles_column, mc_column, target_column, skip_header, number_of_attempts, sample_size, batch_size, keep_top int, progressChan chan<- string) {
 	output_base_fp := input_fp
+
 	file, err := os.Open(input_fp)
 	if err != nil {
 		fmt.Println("Error:", err)
@@ -23,7 +25,9 @@ func processingSSE(input_fp string, number_of_attempts, sample_size, batch_size,
 	}
 	defer file.Close()
 	reader := csv.NewReader(file)
-	_, _ = reader.Read()
+	for range skip_header {
+		_, _ = reader.Read()
+	}
 	records, err := reader.ReadAll()
 	if err != nil {
 		fmt.Println("Error:", err)
@@ -32,8 +36,10 @@ func processingSSE(input_fp string, number_of_attempts, sample_size, batch_size,
 	l := len(records)
 
 	columns := utils.Transpose(records)
-	x := utils.StringToFloat64(columns[1])
-	y := utils.StringToFloat64(columns[2])
+	x := utils.StringToFloat64(columns[mc_column])
+	fmt.Println(x[0])
+	y := utils.StringToFloat64(columns[target_column])
+	fmt.Println(y[0])
 
 	top_pearson_positive := []utils.ValuePair{}
 	top_spearman_positive := []utils.ValuePair{}
@@ -43,6 +49,7 @@ func processingSSE(input_fp string, number_of_attempts, sample_size, batch_size,
 	top_spearman_negative := []utils.ValuePair{}
 	lowest_top_pearson_negative := -math.MaxFloat64
 	lowest_top_spearman_negative := -math.MaxFloat64
+	fmt.Println(number_of_attempts)
 
 	for batch_start := 0; batch_start < number_of_attempts; batch_start += batch_size {
 		batch_attempts := min(batch_size, number_of_attempts-batch_start)
@@ -50,7 +57,7 @@ func processingSSE(input_fp string, number_of_attempts, sample_size, batch_size,
 		randomIndexes := utils.GenerateRandintMatrix(batch_attempts, sample_size, 0, l-1)
 
 		for i, indexes := range randomIndexes {
-			indexes_selected := utils.GetByIndexes(columns[0], indexes)
+			indexes_selected := utils.GetByIndexes(columns[smiles_column], indexes)
 			x_selected := utils.GetByIndexes(x, indexes)
 			y_selected := utils.GetByIndexes(y, indexes)
 
@@ -96,25 +103,30 @@ func processingSSE(input_fp string, number_of_attempts, sample_size, batch_size,
 					}
 				}
 			}
-			progressChan <- batch_start + i + 1
+			// fmt.Println(batch_start + i + 1)
+			progressChan <- "data: " + strconv.Itoa(batch_start+i+1)
 		}
 	}
-	close(progressChan)
 	utils.SaveTopCorrelationsToCSV(top_pearson_positive, output_base_fp, fmt.Sprintf("_corr_%d_%d_pearson+_top%d.csv", number_of_attempts, sample_size, keep_top))
 	utils.SaveTopCorrelationsToCSV(top_pearson_negative, output_base_fp, fmt.Sprintf("_corr_%d_%d_pearson-_top%d.csv", number_of_attempts, sample_size, keep_top))
 	utils.SaveTopCorrelationsToCSV(top_spearman_positive, output_base_fp, fmt.Sprintf("_corr_%d_%d_spearman+_top%d.csv", number_of_attempts, sample_size, keep_top))
 	utils.SaveTopCorrelationsToCSV(top_spearman_negative, output_base_fp, fmt.Sprintf("_corr_%d_%d_spearman-_top%d.csv", number_of_attempts, sample_size, keep_top))
+
+	progressChan <- "res: " + fmt.Sprintf("%s_corr_%d_%d_pearson+_top%d.csv", strings.Replace(output_base_fp, ".csv", "", 1), number_of_attempts, sample_size, keep_top)
+	progressChan <- "res: " + fmt.Sprintf("%s_corr_%d_%d_pearson-_top%d.csv", strings.Replace(output_base_fp, ".csv", "", 1), number_of_attempts, sample_size, keep_top)
+	progressChan <- "res: " + fmt.Sprintf("%s_corr_%d_%d_spearman+_top%d.csv", strings.Replace(output_base_fp, ".csv", "", 1), number_of_attempts, sample_size, keep_top)
+	progressChan <- "res: " + fmt.Sprintf("%s_corr_%d_%d_spearman-_top%d.csv", strings.Replace(output_base_fp, ".csv", "", 1), number_of_attempts, sample_size, keep_top)
+	close(progressChan)
 }
 
-func runProcessing(input_fp string, number_of_attempts, sample_size, batch_size, keep_top int, w http.ResponseWriter) {
-	fmt.Println("asdf")
-	progressChan := make(chan int)
-	go processingSSE(input_fp, number_of_attempts, sample_size, batch_size, keep_top, progressChan)
+func runProcessing(input_fp string, smiles_column, mc_column, target_column, skip_header, number_of_attempts, sample_size, batch_size, keep_top int, w http.ResponseWriter) {
+	progressChan := make(chan string)
+	go processingSSE(input_fp, smiles_column, mc_column, target_column, skip_header, number_of_attempts, sample_size, batch_size, keep_top, progressChan)
 	w.Header().Set("Content-Type", "text/event-stream")
 	w.Header().Set("Cache-Control", "no-cache")
 	w.Header().Set("Connection", "keep-alive")
 	for progress := range progressChan {
-		fmt.Fprintf(w, "data: %d\n\n", progress)
+		fmt.Fprintf(w, "%s\n\n", progress)
 		w.(http.Flusher).Flush()
 	}
 }
@@ -128,7 +140,9 @@ func main() {
 			return
 		}
 		defer file.Close()
-		input_fp := "temp/" + utils.GenerateRandomString(5) + ".csv"
+		// input_fp := "temp/" + utils.GenerateRandomString(5) + ".csv"
+		input_fp := "temp/" + "test" + ".csv"
+
 		out, err := os.Create(input_fp)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
@@ -144,7 +158,19 @@ func main() {
 		sample_size, _ := strconv.Atoi(c.PostForm("sample_size"))
 		batch_size := 10000
 		keep_top, _ := strconv.Atoi(c.PostForm("keep_top"))
-		runProcessing(input_fp, number_of_attempts, sample_size, batch_size, keep_top, c.Writer)
+
+		smiles_column, _ := strconv.Atoi(c.PostForm("smiles_column"))
+		mc_column, _ := strconv.Atoi(c.PostForm("mc_column"))
+		target_column, _ := strconv.Atoi(c.PostForm("target_column"))
+		skip_header, _ := strconv.Atoi(c.PostForm("skip_header"))
+
+		runProcessing(input_fp, smiles_column, mc_column, target_column, skip_header, number_of_attempts, sample_size, batch_size, keep_top, c.Writer)
 	})
+
+	r.GET("/download", func(c *gin.Context) {
+		filePath := c.Query("file")
+		c.File(filePath)
+	})
+
 	r.Run(":8080")
 }
